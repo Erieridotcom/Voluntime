f289c211b5c3063ef02ce8759f018d64bab688a5
Eri <er1aneh23@gmail.com>
2026-03-31 15:22:27 -0600

    utils.ts
    Api

--- artifacts/voluntariado/src/lib/utils.ts
+++ artifacts/voluntariado/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getApiBase(): string {
+  return (import.meta.env.VITE_API_URL ?? "").replace(/\/$/, "")
+}
